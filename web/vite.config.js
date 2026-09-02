0c05d54c119b75742a2882eead73029bc898ee35
EternalHeart <hao131462@qq.com>
2026-09-02 15:57:00 +0800

    chore: 初始化项目工程配置

---
+++ web/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 9999,
+    strictPort: true
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: './'
+})
